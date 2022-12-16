136ae0bdfeea09465ef777fb7ff646300b767772
efezino idisi <enduranceidisi13@gmail.com>
2022-12-16 10:26:53 +0100

    initial project files

---
+++ list_functions.c
@@ -0,0 +1,82 @@
+#include "monty.h"
+
+/**
+ * add_snodeint - add a new node at the beginning
+ * @head: doubly linked list
+ * @n: data
+ *
+ * Return: address of new node or NULL
+ */
+
+stack_t *add_snodeint(stack_t **head, const int n)
+{
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	if (*head)
+		(*head)->prev = new_node;
+	*head = new_node;
+
+	return (new_node);
+}
+
+
+/**
+ * add_snodeint_end - add a new node at end of dlist
+ * @head: doubly linked list
+ * @n: data
+ *
+ * Return: address of new node or NULL
+ */
+
+stack_t *add_snodeint_end(stack_t **head, const int n)
+{
+	stack_t *new_node, *current;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	current = *head;
+	if (current == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		while (current->next)
+			current = current->next;
+		new_node->prev = current;
+		current->next = new_node;
+	}
+	return (new_node);
+}
+
+
+/**
+ * free_slistint - frees a slist_int list
+ * @head: start of list
+ *
+ * Return: nothing
+ */
+
+void free_slistint(stack_t *head)
+{
+	stack_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
