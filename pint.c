37fd0ac30bf3c3ad051283da9d93c9f6e0bd618f
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-12-16 00:10:55 -0800

    fix

---
+++ pint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * pint - prints value at top of the stack
+ * @stack : the stack
+ *
+ * Return: void
+ */
+
+void pint(stack_t *stack)
+{
+	stact_t *current;
+	int line;
+
+	if (stack)
+	{
+		line = 0;
+		current = stack;
+		while (current)
+		{
+			printf("%d\n", current->n);
+			current = current->next;
+			line++;
+		}
+	}
+	else
+	{
+		fprintf(2,"L%d: can't pint, stack empty\n", line);
+	}
+
+}
